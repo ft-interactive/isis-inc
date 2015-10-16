bdc6183ce6bf645f354cddee695903fedfbad668
Callum Locke <callum.locke@gmail.com>
2015-10-16 12:17:54 +0100

    hide fake content while loading

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -66,6 +66,8 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.stories__container').innerHTML = storiesHTML;
   
   document.querySelector('#byline').innerHTML = writeCredits(credits);
+
+  document.body.classList.add('ready');
 });
 
 
